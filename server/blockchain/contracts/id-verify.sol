b5e396e555ee9e3e122d7e172c93952990c1300a
JoelVCrasta <joelcrasta03@proton.me>
2025-05-07 20:08:54 +0530

    added owner check and event emitter

--- server/blockchain/contracts/id-verify.sol
+++ server/blockchain/contracts/id-verify.sol
@@ -2,11 +2,25 @@
 pragma solidity ^0.8.0;
 
 contract IDVerifier {
+    address public owner;
     mapping(bytes32 => bool) public storedHashes;
+    event IDHashAdded(bytes32 indexed hash);
+
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     // Function to store an ID hash
-    function addIDHash(bytes32 hash) external {
+    function addIDHash(bytes32 hash) external onlyOwner {
+        require(!storedHashes[hash], "Hash already exists");
         storedHashes[hash] = true;
+        emit IDHashAdded(hash);
     }
 
     // Function to verify if an ID hash exists
